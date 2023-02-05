bf2312b8aeb1f5d0a6d239a54b7b8c4631625777
pitipat_mac <pitipatw@mit.edu>
2023-02-05 12:52:18 -0500

    Prepare for two-material optimization

--- FEM_truss.jl
+++ FEM_truss.jl
@@ -1,3 +1,6 @@
+include("Utilities.jl")
+include("FEM_utilities.jl")
+
 """
 A, E, f, g, idb, ien, ndf, nel, nen ,nnp ,nsd ,xn ,sL  = data_truss()
 
@@ -8,18 +11,15 @@ A, E, f, g, idb, ien, ndf, nel, nen ,nnp ,nsd ,xn ,sL  = data_truss()
 
 """
 function FEM_truss()
-    include("TopOpt-but-Julia/Utilities.jl")
-    include("TopOpt-but-Julia/FEM_utilities.jl")
 
 #  ---- READ IN DATA -----------------------------------------------------
 # Get data from data_truss()
+# Is there a better way to do this?
 A, E, f, g, idb, ien, ndf, nel, nen ,nnp ,nsd ,xn ,sL  = data_truss()
 # display(ien)
 # ---- NUMBER THE EQUATIONS ---------------------------------------------
 # line 380
 id::Array{Int8},neq = number_eq(idb,ndf,nnp);
-
-
 
 # ---- FORM THE ELEMENT STIFFNESS MATRICES ------------------------------
 # line 324
@@ -63,16 +63,17 @@ Rcomp,idr = reactions(idb,ien,Fe,ndf,nee,nel,nen,nnp);
 # set the plot factor for the thickness of the truss elements
 plot_fac_bar = 1;
 A_min        = 0.01;
-plot_truss(A,A_min,f,idr,ien,nel,nnp,nsd,plot_fac_bar,xn);
+# plot_undeform(A, nodeCoor,eNodes, nNodes,stress)
 
 
 # Sensitivity calculation
-df = zeros(nel,1);
-de = zeros(4,nel) ; 
+# Define empty sensitivity matrices.
+df = zeros(Float64, nel,1) ;
+de = zeros(Float64, 4,nel) ; 
 for i = 1:nel
-    iend = ien(:,i) ;
+    # Initiate Stiffness matrix without YoungModulus. 
     Ke0  = zeros(4,4,nel);
-    Ke0[:,:,i],Te[:,:,i] = Ke_truss(1,E[i],iend,4,nsd,xn);
+    Ke0[:,:,i],Te[:,:,i] = Ke_truss(1,E[i],ien[:,i],nee,nsd,xn);
     nodes = LM[:,i] ; 
     for k = 1:4 
         if nodes[k] != 0
@@ -82,6 +83,27 @@ for i = 1:nel
     end
     df[i,1] = -de[:,i]' * Ke0[:,:,i] * de[:,i];
 end
+# There could be ρ here that multiply the whole sL.
+ρ = 1*ones(Float64, 1, nel)
 
-return [F,d,df,stress]
+dg = ρ .* sL
+
+#This part is for two-material truss topology optimization for minimum compliance.
+# minimize f = Fᵀd
+# subject to 
+# K(Ae, xe) = F
+# add new a new variable xe. Telling how much material there are,
+
+# basically make the variable longer, i.e. from [A1 A2 A3 ... An] into [A1 A2 A3 ... An x1 x2 x3 ... xn]
+# then slice the variable for the input, find sensitivities. then output the objective value. 
+#     Etimber = 11.
+#     Esteel  = 200.
+#     σy_timber = [-8.6 6.6]
+#     σ_steel = [-345. 345.]
+#     ρtimber = 570.
+#     ρsteel = 7870.
+#     ECC_timber = 0.42
+#     ECC_steel  = 1.45
+
+return [F,d,df,dg,stress]
 end
