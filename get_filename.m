0f978f213a05ed83bedbd8c6ac0ece48edebd5a6
pitipatw <pitipatw@mit.edu>
2023-02-10 12:28:46 -0500

    Update file

--- get_filename.m
+++
@@ -1,8 +0,0 @@
-function name = get_filename()
-x = replace(string(datetime) , {':',' ' ,'-'},"_");
-% name = replace(strcat("ver1_", string(vf),"_", string(lc), "_", x)     , '.' , 'dot');
-name = replace(strcat("ver1_", x) , '.' , 'dot');
-
-
-
-
