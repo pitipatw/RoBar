a11836aaa4fe65ea33487d730de77cd460e5a9e5
pitipat_mac <pitipatw@mit.edu>
2023-04-04 12:02:58 -0400

    Proper Removing Areas and Plot

--- test/main.jl
+++ test/main.jl
@@ -1,70 +1,351 @@
-using TopOpt, LinearAlgebra, StatsFuns
-using Makie, GLMakie
-using TopOpt.TrussTopOptProblems.TrussVisualization: visualize
-using StaticArrays
-include("Utilities.jl")
-# 2D
-ndim = 2
+# using TopOpt
+# TopOpt v0.7.2 `https://github.com/pitipatw/TopOpt.jl#master`
+using Makie, GLMakie, CairoMakie
+using JSON
+#using Meshes
+using ColorSchemes
 
-#this part is what you have to do
-node_points, elements, mats, crosssecs, fixities, load_cases = load_truss_json(
-    joinpath(@__DIR__, "tim_$(ndim)d2.json")
-    # joinpath(@__DIR__, "fromGH.json")
-
-)
-
-
-nnx = 10 ; # number of nodes on X
-nny = 10 ; # number of nodes on Y
-L = 10. ;
-H = 10. ;
-# would be nice to able to specify the type of dict
-node_points2, elements2 = groundStruct(nnx,nny,L,H) ;
-
-
-ndim, nnodes, ncells = length(node_points[1]), length(node_points), length(elements)
-
-ndim, nnodes, ncells = length(node_points2[1]), length(node_points2), length(elements2)
-loads = load_cases["0"]
-problem = TrussProblem(
-    Val{:Linear}, node_points2, elements2, loads, fixities, mats, crosssecs
-)
-
-xmin = 0.0001 # minimum density
-x0 = fill(1.0, ncells) # initial design
-p = 4.0 # penalty
-V = 0.8 # maximum volume fraction
-
-solver = FEASolver(Direct, problem; xmin=xmin)
-comp = TopOpt.Compliance(solver)
-
-function obj(x)
-    # minimize compliance
-    return comp(PseudoDensities(x))
-end
-function constr(x)
-    # volume fraction constraint
-    return sum(x) / length(x) - V
+begin
+    include("STM_info_mod.jl")
+    include("STM_factors.jl")
+    include("STM_findPath.jl")
+    include("STM_postProcess.jl")
 end
 
-# setting upmodel
-m = Model(obj)
-#add variable boundary (model , lb, ub)
-addvar!(m, zeros(length(x0)), 2*ones(length(x0)))
-# add constrain
-Nonconvex.add_ineq_constraint!(m, constr)
+# Data input
+filename = "TopOpt1_out"
+"""
+Data Fields
+Area : A list of areas for each element
+Nodes : A dictionary, "5" : [x, y ,z] (string -> list of floats)
+Elements : A dictionary, "element num" : [start end idx in floats]
+Stress : A list of stresses for each element
+"""
+data = JSON.parsefile(filename*".json"::AbstractString; dicttype=Dict, inttype=Float64, use_mmap=true)
 
-options = MMAOptions(; maxiter=5000, tol=Tolerance(; kkt=1e-4, f=1e-4))
-TopOpt.setpenalty!(solver, p)
-@time r = Nonconvex.optimize(
-    m, MMA87(; dualoptimizer=ConjugateGradient()), x0; options=options
-)
+# turn Node_points into Int64 -> [Floats] format.
+begin
 
-@show obj(r.minimizer)
-@show constr(r.minimizer)
-fig = visualize(
-    problem, solver.u, topology = r.minimizer,
-    default_exagg_scale=0.0
-)
-Makie.display(fig)
+    node_points_raw = data["Nodes"]
+    node_points = Dict{Int64, Tuple{Float64,Float64,Float64}}()
+    for (k,v) in node_points_raw 
+        node_points[parse(Int64,k)] = Tuple{Float64,Float64,Float64}(v)
+    end
+end
 
+# node_points, elements, mats, crosssecs, fixities, load_cases = load_truss_json(joinpath(@__DIR__, "Tester_ver2.json"))
+areas = convert(Array{Float64,1}, data["Area"]) ;
+σ_raw = convert(Array{Float64,1},data["Stress"]) ;
+
+# turn elements into Int64 -> Int64 format.
+begin
+    elements_raw = data["Elements"] ;
+    elements_raw_converted = Dict{Int64, Tuple{Int64, Int64}}();
+    for (k,v) in elements_raw 
+        elements_raw_converted[parse(Int64,k)] = Tuple{Int64, Int64}(v) ;
+    end
+end
+
+area_filter = areas .> 0 ;
+pos_areas = areas[area_filter] ;
+σ = σ_raw[areas .> 0 ] ;
+
+elements = Dict{Int64, Tuple{Int64, Int64}}()
+counter = 0 
+for i in eachindex( area_filter ) 
+    if area_filter[i] == true
+        counter += 1
+        elements[counter] = elements_raw_converted[i]
+    end
+end
+
+# Material properties
+begin
+    fc′ = 30. # concrete strength [MPa]
+    Ec = 4700.0*sqrt(fc′)
+    Es = 200000. # steel strength [MPa]
+end
+
+println("Inputs Pass Successfully!")
+
+#STM starts here. 
+
+Amin = 0.001
+
+# Get node-element info
+node2elements, node2element_scores ,node2element_areas_raw, node2forces = nodeElementInfo(pos_areas, σ ,elements);
+node2element_areas, mod_pos_areas = removeHanging(pos_areas, node2elements, node2element_areas_raw)
+
+#visualize the plot before and after checkNodeElement
+#plot the truss structure before and after the modifications. 
+
+#plot truss structure
+scale = 5.0
+truss0 = Figure(resolution = (1000, 1000));
+axis0 = Axis(truss0[1,1], xlabel = "x", ylabel = "y", aspect=DataAspect());
+axis1 = Axis(truss0[2,1] , xlabel = "x", ylabel = "y", aspect=DataAspect()); 
+#axis equal
+for i in eachindex(elements)
+    element = elements[i]
+    node1 = node_points[element[1]]
+    node2 = node_points[element[2]]
+    @show pos_areas[i]
+    @show mod_pos_areas[i]
+    if σ[i] > 0 
+    lines!(axis0, [node1[1], node2[1]], [node1[2], node2[2]], linewidth = pos_areas[i]*scale , color=:blue)
+    lines!(axis1, [node1[1], node2[1]], [node1[2], node2[2]], linewidth = mod_pos_areas[i]*scale, color=:blue)
+    else 
+
+    lines!(axis0, [node1[1], node2[1]], [node1[2], node2[2]], linewidth = pos_areas[i]*scale , color=:red)
+    lines!(axis1, [node1[1], node2[1]], [node1[2], node2[2]], linewidth = mod_pos_areas[i]*scale, color=:red)
+    end
+
+end
+#plot supports 
+# for (k,v) in fixities
+#     @show node = k
+#     x_res = v[1] 
+#     y_res = v[2]
+#     @show xval = node_points[node][1]
+#     @show yval = node_points[node][2]
+#     if x_res 
+#         scatter!(axis0, xval-1, yval, marker = :rtriangle, color = :red)
+#     end
+#     if y_res
+#         scatter!(axis0, xval, yval-1, marker = :utriangle, color = :red)
+#     end
+# end
+
+f0 = display(truss0)
+
+#explain the structure of node2elements and node2element_scores
+
+node2elements
+
+# Get node score (CCC, CCT, CTT)
+score = getScore(node2element_scores)
+
+list_of_betan = getBetaN(score)
+
+#this might be useless
+elements_betan = getElementsBetan(elements, list_of_betan)
+element_forces = σ .* pos_areas
+#check strut and ties capacity
+element_capacity_status = checkStrutAndTie(pos_areas, element_forces, 30.)
+
+#check node capacity
+node_capacity_status = checkNodes(node2forces,node2elements, pos_areas,fc′)
+
+# get rid of hanging node
+
+possible_starting_nodes = feasibleStartingPoints(node2element_areas)
+Makie.inline!(true)
+f0 = GLMakie.Figure(resolution = (1000, 1000));
+ax = GLMakie.Axis3(f0[1,1], aspect = :data)
+for (k0,v0) in elements
+    x1 = node_points[v0[1]][1]
+    y1 = node_points[v0[1]][2]
+    z1 = node_points[v0[1]][3]
+    x2 = node_points[v0[2]][1]
+    y2 = node_points[v0[2]][2]
+    z2 = node_points[v0[2]][3]
+    lines!(ax, [x1,x2], [y1,y2], [z1,z2], color = :gray, linewidth = 0.5)
+end
+display(f0)
+#label each node and element number
+for (k0,v0) in node_points
+    text!(ax, v0[1], v0[2], v0[3], string(k0), color = :black, textsize = 10)
+end
+#label each element
+for (k0,v0) in elements
+    x1 = node_points[v0[1]][1]
+    y1 = node_points[v0[1]][2]
+    z1 = node_points[v0[1]][3]
+    x2 = node_points[v0[2]][1]
+    y2 = node_points[v0[2]][2]
+    z2 = node_points[v0[2]][3]
+    text!(ax, (x1+x2)/2, (y1+y2)/2, (z1+z2)/2, string(k0), color = :black, textsize = 10)
+end
+# find the Path
+# Iterate every points as a starting point, and every element connected to the point as a starting element
+
+#this is for findPath version2
+fixed_area =  0.3
+possible_paths = Dict()
+for i in eachindex(possible_starting_nodes)
+    start_node = possible_starting_nodes[i]
+    #get possible starting elements
+    possible_start_elements= node2elements[start_node]
+    for j in eachindex(node2elements[start_node])
+        # println("j:" , j)
+        # println("start_node:", start_node)
+        if possible_start_elements[j] ∉ node2elements[start_node]
+            println("HI")
+            continue
+        end
+        # if node2element_areas[start_node][1] <= 0 
+        #     println("HI")
+        #     continue
+        # end
+        start_element = Int(possible_start_elements[j])
+        # println("start node: ", start_node)
+        # println("Start element: ", start_element)
+        # result = findPath(node2element_areas, node2elements,elements, start_node,start_element)
+        println("startfindingpath2")
+        result = findPath2(node2element_areas, node2elements,elements, start_node,start_element, fixed_area)
+        println("endfindingpath2")
+        # passed_points = result[1] # a list of points
+        # passed_elements = result[2] # a list of elements
+        possible_paths[(i,j)] = result
+    end
+end
+println("Number of possible Paths: ", length(possible_paths))
+
+# now the possible_paths dictionary contains the all of the possible paths
+Makie.inline!(false)
+
+
+begin
+f = Makie.Figure(resolution = (1000, 1000));
+#collecting the points for plot
+# loop each possible path to plot
+    #gonna do moving graph thing :)
+nr = 5
+nc = 5
+track_row = 1
+track_col = 1 
+plt_counter = 0
+
+for (k,v) in possible_paths
+    # if plt_counter > track_row*track_col
+        
+    #reset ptx pty ptz
+    ptx = zeros(length(v[1])) # v[1] is a list of points
+    pty = zeros(length(v[1]))
+    ptz = zeros(length(v[1]))
+    for i in eachindex(v[1])
+        println("i: ", i)
+        println("v[1][i]: ", v[1][i])
+        ptx[i] = node_points[v[1][i]][1]
+        pty[i] = node_points[v[1][i]][2]
+        ptz[i] = node_points[v[1][i]][3]
+    end
+    ax = Axis3(f[track_row, track_col], title = "Truss Path $k",
+    xlabel = "x", ylabel = "y", zlabel = "z", aspect = (1,1/2,1))
+
+    ax.azimuth[] = -pi/2
+    ax.elevation = pi/2
+
+    # t is for color
+    t = range(0, stop=1,length = length(ptx))
+
+    lines!(ax, ptx, pty, ptz, color = t, linewidth = 3, 
+        colormap = ColorSchemes.magma.colors)
+
+    start_point = (ptx[1] , pty[1] , ptz[1])
+    end_point   = (ptx[end] , pty[end] , ptz[end])
+
+    # println("Start Point: ", start_point)
+    # println("End Point: ", end_point)
+
+    scatter!(ax, start_point, color=:red, markersize = 15)
+    #text!(ax,start_point, text = "Start", color = :red)
+    scatter!(ax, end_point, color = :green, markersize = 10)
+    #text!(ax,end_point, color = :green , text = "End")
+
+    #plot entire truss
+    for (k0,v0) in elements
+        x1 = node_points[v0[1]][1]
+        y1 = node_points[v0[1]][2]
+        z1 = node_points[v0[1]][3]
+        x2 = node_points[v0[2]][1]
+        y2 = node_points[v0[2]][2]
+        z2 = node_points[v0[2]][3]
+        lines!(ax, [x1,x2], [y1,y2], [z1,z2], color = :gray, linewidth = 0.5)
+    end
+    if track_col == nc
+        if track_row == nr
+            break
+        end
+        track_row += 1
+        track_col = 1
+    else
+        track_col += 1
+    end
+
+end
+cb = Colorbar(f[:, nc+1] ,colormap = ColorSchemes.magma.colors )
+display(f)
+end
+
+display(f)
+# ptx = zeros(length(passed_points))
+# pty = zeros(length(passed_points))
+# ptz = zeros(length(passed_points))
+# @time for i in eachindex(passed_points)
+#     ptx[i] = node_points[passed_points[i]][1]
+#     pty[i] = node_points[passed_points[i]][2]
+#     ptz[i] = node_points[passed_points[i]][3]
+# end
+
+passed_element_areas = zeros(length(passed_elements))
+
+for i in eachindex(passed_elements)
+    passed_element_areas[i] =pos_areas[Int(passed_elements[i])]
+end
+
+
+
+#post processing
+#pushing areas to the next available size
+available_sizes =  [ 1. 2. 3.][:]
+mod_list_of_areas = postProcess(pos_areas, available_sizes)
+
+
+pos_areas[Int.(passed_elements)]
+passed_element_areas
+#We have to check this for strain.
+
+
+#This is for visualization
+f = Figure(resolution = (800, 800))
+ax = Axis3(f[1, 1], title = "Truss Path",
+        xlabel = "x", ylabel = "y")
+points = Point3f.(ptx,pty,ptz)
+#points = [ptx, pty, ptz]
+t = range(0, stop=1,length = length(ptx))
+
+start_point = points[1]
+end_point = points[end]
+lines(points,
+        colormap = ColorSchemes.magma.colors,
+        color = t)
+scatter!(start_point,color=:red, markersize = 10)
+text!(start_point, text = "Start", color = :red)
+scatter!(end_point, color = :green, markersize = 10)
+text!(end_point, text = "End", color = :green)
+
+f2 = Figure(resolution = (800, 800))
+ax2 = Axis3(f2[1, 1], title = "Truss Path2",xlabel = "x", ylabel = "y")
+
+for i in eachindex(elements)
+    
+    ptx1 = node_points[elements[i][1]][1]
+    pty1 = node_points[elements[i][1]][2]
+    ptz1 = node_points[elements[i][1]][3]
+    ptx2 = node_points[elements[i][2]][1]
+    pty2 = node_points[elements[i][2]][2]
+    ptz2 = node_points[elements[i][2]][3]
+    points = Point3f.([ptx1; ptx2] , [pty1; pty2], [ptz1; ptz2])
+    println(points)
+    if i in passed_elements
+        println("HI")
+        lines!(points,color = :red, linewidth = 50)
+    else
+        println("No HI")
+        lines!(points,color = :black, linewidth = 10)
+    end
+end
+f2
+
+# this part is for plotting another plot with color as the utilization ratio
