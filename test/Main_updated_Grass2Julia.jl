68ee550b2bf4961a6b4946e39e31a9757ba3d228
pitipat_mac <pitipatw@mit.edu>
2023-07-18 23:29:24 -0700

    mud update 1

--- test/Main_updated_Grass2Julia.jl
+++ test/Main_updated_Grass2Julia.jl
@@ -15,6 +15,7 @@ end
 # 14 March 2023
 
 include("GS.jl")
+GLMakie.activate!(inline=false)
 #add LinearAlgebra
 server = WebSockets.listen!("127.0.0.1", 2000) do ws
     for msg in ws
@@ -24,7 +25,7 @@ server = WebSockets.listen!("127.0.0.1", 2000) do ws
         data = JSON.parse(msg)
         stage = data["stage"]
         date = data["date"]
-        filename = date*"Kuka1"
+        filename = date * "Muddy"
 
         if stage == "GS"
             println("Entering GroundStructure creation stage...")
@@ -32,11 +33,11 @@ server = WebSockets.listen!("127.0.0.1", 2000) do ws
             node_points = Dict{Int64,Vector{Float64}}()
             node_dummy = data["nodes"]
             for i in eachindex(node_dummy)
-                node_points[parse(Int64,i)] = Vector{Float64}(node_dummy[i])
+                node_points[parse(Int64, i)] = Vector{Float64}(node_dummy[i])
             end
             Lmax = data["Lmax"]
-                
-            GS = getGS(node_points,Lmax)
+
+            GS = getGS(node_points, Lmax)
             #write GS to JSON
             msg = JSON.json(GS)
             println("Ground Structure created!")
@@ -44,11 +45,12 @@ server = WebSockets.listen!("127.0.0.1", 2000) do ws
             send(ws, msg)
         elseif stage == "Opt"
             println("Entering Topology Optimization stage...")
-            open(joinpath(@__DIR__, filename*".json"), "w") do f
-            write(f, msg)
+            open(joinpath(@__DIR__, filename * ".json"), "w") do f
+                write(f, msg)
+            end
             println("A json file written Successfully")
-            
-            node_points, elements, mats, crosssecs, fixities, load_cases = load_truss_json(joinpath(@__DIR__, filename*".json"))
+
+            node_points, elements, mats, crosssecs, fixities, load_cases = load_truss_json(joinpath(@__DIR__, filename * ".json"))
 
             ndim, nnodes, ncells = length(node_points[1]), length(node_points), length(elements)
             loads = load_cases["0"]
@@ -66,28 +68,28 @@ server = WebSockets.listen!("127.0.0.1", 2000) do ws
                 node2 = node_points[element[2]]
                 elements_L[i] = norm(node1 .- node2)
             end
-            
+
 
             # setting up the problem
             xmin = 0.0001 # minimum density
             V = data["maxVf"]
 
-            x0 = V.*fill(1, ncells) # initial design
+            x0 = V .* fill(1, ncells) # initial design
             # might not work on every objective function.
             # change to 1.0, and allow upper boundary for x,
             # so it reflects the actual cross sections area.
             p = 4.0 # penalty
-    
+
             solver = FEASolver(Direct, problem; xmin=xmin)
             comp = TopOpt.Compliance(solver)
-            
+
             function obj(x)
                 # minimize compliance
                 return comp(PseudoDensities(x))
             end
             function constr(x)
                 # volume fraction constraint
-                return sum(x .* elements_L)/sum(elements_L) - V
+                return sum(x .* elements_L) / sum(elements_L) - V
             end
 
             # to do 
@@ -99,7 +101,7 @@ server = WebSockets.listen!("127.0.0.1", 2000) do ws
             addvar!(m, zeros(length(x0)), ones(length(x0)))
             # add constrain
             Nonconvex.add_ineq_constraint!(m, constr)
-            options = MMAOptions(; maxiter=500, tol=Tolerance(; kkt=1e-4, f=1e-4))
+            options = MMAOptions(; maxiter=1000, tol=Tolerance(; kkt=1e-3, f=1e-3))
             TopOpt.setpenalty!(solver, p)
 
             # Run the optimization
@@ -107,44 +109,44 @@ server = WebSockets.listen!("127.0.0.1", 2000) do ws
                 m, MMA87(; dualoptimizer=ConjugateGradient()), x0; options=options
             )
             # Set r to global, so r could be checked again later.
-            println("Optimization finished!")  
+            println("Optimization finished!")
             global r
             @show obj(r.minimizer)
             @show constr(r.minimizer)
 
             ts = TrussStress(solver)
             σ1 = ts(PseudoDensities(r.minimizer))
-            color = [σ1[i]>0 ? 1 : -1 for i in eachindex(σ1)]
+            color = [σ1[i] > 0 ? 1 : -1 for i in eachindex(σ1)]
             id = 0:1:(length(x0)-1)
             global results = Dict()
 
             # create JSON to send back the information
-            results["Area"] =  r.minimizer
+            results["Area"] = r.minimizer
             results["Stress"] = σ1
             results["Elements"] = elements
             results["Nodes"] = node_points
             msg = JSON.json(results)
-            send(ws, msg)  
+            send(ws, msg)
 
             # write savepath 
             savepath = joinpath(@__DIR__, filename * "_out.json")
             # save
-            open(joinpath(@__DIR__,savepath), "w") do f
+            open(joinpath(@__DIR__, savepath), "w") do f
                 write(f, msg)
             end
             println("json output file written Successfully")
-            
+
             #Truss visualization
             fig = visualize(
-                problem; u =solver.u, topology=r.minimizer, cell_colors = color, 
-                colormap = ColorSchemes.brg,
-                default_exagg_scale=0.0 #,default_element_linewidth_scale = 3.0
-                ,default_support_scale = 0.0
-                )
+                problem; u=solver.u, topology=r.minimizer, cell_colors=color,
+                colormap=ColorSchemes.brg,
+                default_exagg_scale=0.0, default_support_scale=0.0
+            )
             Makie.display(fig)
+
         end
     end
-    end
 end
+
 
 close(server)
